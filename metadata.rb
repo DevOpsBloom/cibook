7616cb82eef4b73b75d9322b82575009b1a1ae8b
AbhijithSetty2 <Abhijith.Setty2@target.com>
2021-07-12 23:39:25 -0500

    Add supermarket

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,12 @@
+source 'https://supermarket.chef.io'
 name 'cibook'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures cibook'
-version '0.3.2'
+version '0.3.3'
 chef_version '>= 15.0'
+
 depends 'chef-vault'
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
